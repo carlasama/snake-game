fcaf49f4dbdf6d50ee6fbec395adc3b0503acc81
Carla Samaniego <84520565+carlasama@users.noreply.github.com>
2021-06-16 16:21:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-// Carregando o snake com uma variável chamada "canvas".
-let canvas = document.getElementById('snake');
-// Contexto é a reinderização do Canvas que vai trabalhar com um plano 2D.
-let context = canvas.getContext('2d'); 
-// 32 pixels cada quadrado.
-let box = 32;
-let snake = []; // Criando um array.
 
-// Passando o que vai ter dentro do array.
-snake[0] = { // Definindo a posição.
-    x: 8 * box, // Dando um tamanho.
+let canvas = document.getElementById('snake');
+
+let context = canvas.getContext('2d'); 
+
+let box = 32;
+let snake = []; 
+
+snake[0] = { 
+    x: 8 * box, 
     y: 8 * box
 }
 
-// Variável responsável pela a direção.
 let direction = "right";
-/**
- * Math.floor retira a parte flutuante.
- * Math.random varia as posições da comida no cenário do jogo.
-*/
+
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
 }
 
-/**
- * Criando a função do Background.
- * Update: Alterado a cor de background para preto. 
- */
+
 function criarBG(){
     // Definindo a cor. | fillstyle trabalha com o estilo do canvas.
     context.fillStyle = "#3b3b3b";
@@ -34,32 +26,28 @@ function criarBG(){
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-/**
- * Função responsável pela a criação da cobrinha.
- * Update: Adicionei -1 no box para criar um espaço entre os quadrados da cobrinha.
- */
 function criarCobrinha(){
-    // for vai percorrer todo o tamanho do array e vai incrementar.
+  
     for(i=0; i < snake.length; i++){
-        // Definindo a cor.
+        
         context.fillStyle = "#000000";
-        // Passando o tamanho.
+        
         context.fillRect(snake[i].x, snake[i].y, box-2, box-2);
-    } // Fecha for
-} // Fecha função criarCobrinha
+    } 
+} 
 
-// Função responsável pela a criação da comida.
+
 function drawFood(){
-    // Definindo a cor da comida.
+    
     context.fillStyle = "#993399";
-    // Passando as posições quando o fillRect ir desenhar.
+    
     context.fillRect(food.x, food.y, box, box)
 }
 
-// Evento de clique vai pegar a tecla e dar update.
+
 document.addEventListener('keydown', update);
 
-// Informando o código da tecla e criando a regra que não pode ser na direção oposta.
+
 function update (event){
     if(isSnakeOffScreen()) return;
     if(event.keyCode == 37 && direction != "right") direction = "left";
@@ -68,7 +56,7 @@ function update (event){
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
-// Informa se a cobrinha saiu fora da tela
+
 function isSnakeOffScreen() {
     return (snake[0].x > 15 * box 
         || snake[0].x < 0 
@@ -76,13 +64,9 @@ function isSnakeOffScreen() {
         || snake[0].y < 0)
 }
 
-// Função que carrega partes do jogo.
 function iniciarJogo(){
 
-    /** 
-     * for para verificar se as posição estão se encontrando i - é o corpo da cobrinha.
-     * Caso se encontrem, exibirá um alert informando que perdeu. 
-    */
+
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
@@ -90,10 +74,7 @@ function iniciarJogo(){
         }
     }
 
-    /**
-     * Criando a regra de ultrapassagem da tela(cenário) para voltar ao ponto incial.
-     * Impedindo que a cobrinha suma!
-    */
+ 
     if (isSnakeOffScreen()) {
         if(direction == "right") snake[0].x = 0;
         if(direction == "left") snake[0].x = 16 * box;
@@ -101,7 +82,6 @@ function iniciarJogo(){
         if(direction == "up") snake[0].y = 16 * box;
     }
 
-    // Carrega as funções.
     criarBG();
     criarCobrinha();
     drawFood();
@@ -109,22 +89,15 @@ function iniciarJogo(){
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    /** Coordenadas  - Movimento
-     * Exemplo: Se a cobrinha ir para o lado direito, adiciona um quadrado, 
-     * caso vá para esquerda, diminue um quadrado.
-    */
+
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    /**
-     * Condição - Cobrinha comendo a comida.
-     * Caso as posições da cobrinha e comida sejam diferentes, retira o último elemento.
-     * Se não, ela aumenta e comida surge novamente aleatoriamente.
-     */
+
     if(snakeX != food.x || snakeY != food.y){
-        // Retirando o último elemento do array.
+
         snake.pop();
     }else{food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
@@ -135,9 +108,7 @@ function iniciarJogo(){
         y: snakeY
     }
 
-    // Unshift - Acrescentando um elemento sempre na frente.
     snake.unshift(newHead);
 }
 
-// A cada 100 milisegundos o iniciarJogo vai estar sendo renovado caso trave. 
 let jogo = setInterval(iniciarJogo, 100);
